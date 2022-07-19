88c91de228b2e73dd176305a7ee04ae2fbf509bc
Shreesh Kulkarni <kshreesh5@gmail.com>
2022-07-19 16:00:34 +0530

    Update slave.ino

--- I2C comm/slave/slave.ino
+++ I2C comm/slave/slave.ino
@@ -1,15 +1,26 @@
 //code for Slave(Arduino nano)
 
 #include <Wire.h>
+int tON=0;
+int tOFF=0;
+int Tperiod=0;
+int freq=0;
+int hallpin;
 
 void setup()
 {
   Wire.begin(2);                // join i2c bus with address #2
-  Wire.onRequest(requestEvent); // register event
+  pinMode(hallpin,OUTPUT);
+  Wire.onRequest(requestEvent);
+  // register event
 }
 
 void loop()
 {
+  tON=pulseIn(hallpin,HIGH);
+  tOFF=pulseIn(hallpin,LOW);
+  Tperiod=tON + tOFF;
+  freq= 1000000.0/ Tperiod;
   delay(100);
 }
 
@@ -17,9 +28,8 @@ void loop()
 // this function is registered as an event, see setup()
 void requestEvent()
 {
-  Wire.write("SPEED OF VEHICLE:"); // respond with message of 6 bytes
-  Wire.write("10km/h");
-  Wire.write("\n");
+  // respond with frequency message
+  Wire.write(freq);
   delay(100);
                        // as expected by master
 }
